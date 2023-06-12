6f42bf699ce8199814b97e06499981e46fcf035d
Paulo Henrique <paulohenrique.pimentel@gmail.com>
2023-06-12 19:38:02 -0300

    feat: create the base structure

---
+++ lib/core/theme/constants/app_sizes.dart
@@ -0,0 +1,15 @@
+class AppSizes {
+  AppSizes._();
+
+  static const double s4 = 4.0;
+  static const double s8 = 8.0;
+  static const double s16 = 16.0;
+  static const double s24 = 24.0;
+  static const double s32 = 32.0;
+  static const double s40 = 40.0;
+  static const double s48 = 48.0;
+  static const double s56 = 56.0;
+  static const double s64 = 64.0;
+  static const double s72 = 72.0;
+  static const double s80 = 80.0;
+}
