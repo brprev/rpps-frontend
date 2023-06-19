ae3c0ef7171594c4794fa795972e3bd3ff8713ff
Paulo Henrique <paulohenrique.pimentel@gmail.com>
2023-06-19 18:15:12 -0300

    feat: create theme managers

---
+++ lib/core/theme/managers/colors_manager.dart
@@ -0,0 +1,39 @@
+import 'package:flutter/material.dart';
+
+abstract class IAppColors {
+  AppColors get colors;
+
+  late ColorScheme colorScheme;
+}
+
+class AppColors implements IAppColors {
+  @override
+  final colors = AppColors();
+
+  @override
+  late ColorScheme colorScheme;
+
+  AppColors() {
+    colorScheme = const ColorScheme(
+      background: Colors.white,
+      brightness: Brightness.light,
+      error: Colors.red,
+      onBackground: Colors.white,
+      onError: Colors.white,
+      onInverseSurface: Colors.white,
+      onPrimary: Colors.black,
+      onPrimaryContainer: Colors.white,
+      onSurface: Colors.black,
+      onSecondary: Colors.pink,
+      onSecondaryContainer: Colors.pink,
+      onSurfaceVariant: Colors.pink,
+      onTertiary: Colors.pink,
+      onTertiaryContainer: Colors.pink,
+      outline: Colors.black,
+      primary: Colors.blueGrey,
+      secondary: Colors.pink,
+      surface: Colors.pink,
+      surfaceVariant: Colors.pink,
+    );
+  }
+}
