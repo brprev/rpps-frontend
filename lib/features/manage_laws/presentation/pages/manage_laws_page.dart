63aac8d968e10e6ab441b12c0999636202ae86e7
Paulo Henrique <paulohenrique.pimentel@gmail.com>
2023-06-26 21:47:26 -0300

    feat: add default pages

---
+++ lib/features/manage_laws/presentation/pages/manage_laws_page.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+class ManageLawsPage extends StatelessWidget {
+  const ManageLawsPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold(
+      body: Center(
+        child: Text('Gerenciar leis'),
+      ),
+    );
+  }
+}
